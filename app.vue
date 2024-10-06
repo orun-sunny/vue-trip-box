f7904b01407c21bd1e01dd7d1d502320fe2fd46d
Arafat Islam <arafatkn@gmail.com>
2024-10-06 12:32:33 +0600

    tailwindcss install

--- app.vue
+++ app.vue
@@ -1,6 +1,5 @@
 <template>
-  <div>
-    <NuxtRouteAnnouncer />
-    <NuxtWelcome />
-  </div>
+  <h1 class="text-3xl text-center font-bold underline mt-10">
+    Hello world!
+  </h1>
 </template>
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,4 +3,11 @@ export default defineNuxtConfig({
   compatibilityDate: '2024-04-03',
   devtools: { enabled: true },
   ssr: true,
+  css: ['~/assets/css/main.css'],
+  postcss: {
+    plugins: {
+      tailwindcss: {},
+      autoprefixer: {},
+    },
+  },
 });
